db6fe85347c0ba0ffdf5397191c2c3ce243c1d68
Wolfgang G <wolfgang.gledhill@education.nsw.gov.au>
2026-05-22 07:45:21 +1000

    WIP: Set up

---
+++ Assets/Scripts/enemyController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+    // Movement
+    public float TrueSpeed = 8f;
+    public float desiredSpeed;
+
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+    private Transform thisPlayer;
+    private Rigidbody playerRigidbody;
+    
+    void Start()
+    {
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        GameObject playerObj = GameObject.FindWithTag("Player");
+        
+
+        if (playerObj == null || navMeshAgent == null)
+        {
+            enabled = false;
+            return;
+        }
+
+        thisPlayer = playerObj.transform;
+        playerRigidbody = playerObj.GetComponent<Rigidbody>();
+
+        desiredSpeed = TrueSpeed;
+        navMeshAgent.speed = desiredSpeed;
+
+        
+    }
+
+    void Update()
+    {
+        if (thisPlayer == null || navMeshAgent == null)
+        {
+            Transform target = thisPlayer;
+            navMeshAgent.SetDestination(target);
+            navMeshAgent.speed = desiredSpeed;
+        } else
+        {
+            return;
+        }
+    }
+}
